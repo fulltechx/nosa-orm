78cfd247d888475e5c1b4263b2b45eb114c17ea7
SVNKVN9 <sundaynight.1919@gmail.com>
2025-05-24 06:22:18 +0000

    setup project

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+export default {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 4,
+  arrowParens: "always",
+  endOfLine: "lf",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+};
